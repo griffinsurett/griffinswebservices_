cd284dedfaa5eb976b0497c73845242c176488fb
griffinjoshs <leveloutusa@gmail.com>
2025-12-01 15:47:36 -0500

    new slider and better section buttons

--- src/components/ThemeControls/AccentPicker.tsx
+++ src/components/ThemeControls/AccentPicker.tsx
@@ -36,7 +36,7 @@ export default function AccentPicker() {
 
       {open && (
         <div
-          className="absolute top-full mt-2 right-0 sm:left-0 sm:right-auto faded-bg rounded-xl p-2 lg:p-3 flex space-x-2 lg:space-x-3 overflow-x-auto hide-scrollbar shadow-lg z-50 w-max max-w-[calc(100vw-2.5rem)] sm:max-w-none"
+          className="absolute top-full mt-1 lg:mt-2 right-0 sm:left-0 sm:right-auto faded-bg rounded-xl p-2 lg:p-3 flex space-x-2 lg:space-x-3 overflow-x-auto hide-scrollbar shadow-lg z-50 w-max max-w-[calc(100vw-2.5rem)] sm:max-w-none"
         >
           {accents.map((color) => (
             <SquareCheckbox
