9281e5c6d9fb6e20353a86f38979771f78030658
griffinjoshs <leveloutusa@gmail.com>
2025-12-01 15:35:08 -0500

    new slider and better section buttons

--- src/components/ThemeControls/ThemeControls.tsx
+++ src/components/ThemeControls/ThemeControls.tsx
@@ -15,7 +15,8 @@ export default function ThemeControls({ className = "" }: ThemeControlsProps) {
     <div
       ref={ref}
       className={[
-        "flex items-center gap-1.5",
+        "relative flex items-center gap-1.5",
+        "sm:absolute sm:top-1/2 sm:left-1/2 sm:-translate-x-1/2 sm:-translate-y-1/2",
         "transition-opacity duration-300 ease-in-out z-999999",
         hidden
           ? "opacity-0 pointer-events-none"
